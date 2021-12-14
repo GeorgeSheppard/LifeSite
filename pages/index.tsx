d81040f008e1b517b54bdc4f2a5e3abf2a6cdb78
George <georgesheppard98@gmail.com>
2021-12-14 17:17:48 +0000

    Added lint pre commit hook and some packages

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,9 @@
 import Box from "@mui/material/Box";
-import Button from "@mui/material/Button";
-import Card from "@mui/material/Card";
-import CardActions from "@mui/material/CardActions";
-import CardContent from "@mui/material/CardContent";
-import CardMedia from "@mui/material/CardMedia";
 import Container from "@mui/material/Container";
 import Grid from "@mui/material/Grid";
-import Stack from "@mui/material/Stack";
 import Typography from "@mui/material/Typography";
 import * as React from "react";
+import NavigatorCard from "../components/navigator_card";
 
 const cards = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 
@@ -31,7 +26,7 @@ export default function Home(props: any) {
             color="text.primary"
             gutterBottom
           >
-            Album layout
+            MyLife
           </Typography>
           <Typography
             variant="h5"
@@ -39,19 +34,9 @@ export default function Home(props: any) {
             color="text.secondary"
             paragraph
           >
-            Something short and leading about the collection below—its contents,
-            the creator, etc. Make it short and sweet, but not too short so
-            folks don&apos;t simply skip over it entirely.
+            My personal website, with my projects. Some help me with my everyday
+            life, and some are just ideas that I wanted to make.
           </Typography>
-          <Stack
-            sx={{ pt: 4 }}
-            direction="row"
-            spacing={2}
-            justifyContent="center"
-          >
-            <Button variant="contained">Main call to action</Button>
-            <Button variant="outlined">Secondary action</Button>
-          </Stack>
         </Container>
       </Box>
       <Container sx={{ py: 8 }} maxWidth="md">
@@ -59,36 +44,12 @@ export default function Home(props: any) {
         <Grid container spacing={4}>
           {cards.map((card) => (
             <Grid item key={card} xs={12} sm={6} md={4}>
-              <Card
-                sx={{
-                  height: "100%",
-                  display: "flex",
-                  flexDirection: "column",
-                }}
-              >
-                <CardMedia
-                  component="img"
-                  sx={{
-                    // 16:9
-                    pt: "56.25%",
-                  }}
-                  image="https://source.unsplash.com/random"
-                  alt="random"
-                />
-                <CardContent sx={{ flexGrow: 1 }}>
-                  <Typography gutterBottom variant="h5" component="h2">
-                    Heading
-                  </Typography>
-                  <Typography>
-                    This is a media card. You can use this section to describe
-                    the content.
-                  </Typography>
-                </CardContent>
-                <CardActions>
-                  <Button size="small">View</Button>
-                  <Button size="small">Edit</Button>
-                </CardActions>
-              </Card>
+              <NavigatorCard
+                title="Title"
+                description="This is a media card. You can use this section to describe
+                    the content."
+                imageSrc="/images/plants.jpg"
+              />
             </Grid>
           ))}
         </Grid>
