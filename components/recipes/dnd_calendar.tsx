577a2279584a2f1863ee9092cbcb80b4001ce417
George Sheppard <georgesheppard98@gmail.com>
2022-07-16 14:14:32 +0100

    Latest implementation of drag and drop

--- components/recipes/dnd_calendar.tsx
+++ components/recipes/dnd_calendar.tsx
@@ -10,9 +10,7 @@ import {
   addOrUpdatePlan,
   removeFromPlan,
   DateString,
-  IDailyMealPlan,
   IMealPlanItem,
-  IMealPlanState,
 } from "../../store/reducers/food/meal_plan";
 import { useDispatch } from "react-redux";
 import Button from "@mui/material/Button";
@@ -20,6 +18,7 @@ import ButtonGroup from "@mui/material/ButtonGroup";
 import PersonIcon from "@mui/icons-material/Person";
 import Tooltip from "@mui/material/Tooltip";
 import DeleteIcon from "@mui/icons-material/Delete";
+import { DroppableCard } from "./droppable_card";
 import {
   Dispatch,
   MouseEvent,
@@ -36,9 +35,7 @@ export interface ICalendarRowProps {
 export const Planner = (props: ICalendarRowProps) => {
   const { setSelected, selected } = props;
   const [lastSelected, setLastSelected] = useState<DateString | null>(null);
-  const mealPlan = useAppSelector(
-    (store) => store.mealPlan.plan
-  );
+  const mealPlan = useAppSelector((store) => store.mealPlan.plan);
 
   // Toggles selection onClick
   const onClick = useCallback(
@@ -103,141 +100,5 @@ export const Planner = (props: ICalendarRowProps) => {
         );
       })}
     </Grid>
-  );
-};
-
-const DroppableCard = (props: {
-  day: DateString;
-  selected: boolean;
-  onClick: (day: DateString) => (event: MouseEvent<HTMLDivElement>) => void;
-}) => {
-  const { day, selected, onClick } = props;
-  const meals = useAppSelector(store => store.mealPlan.plan[day])
-  const dispatch = useDispatch();
-
-  const toggleOnClick = useCallback(
-    (event: MouseEvent<HTMLDivElement>) => {
-      onClick(day)(event);
-    },
-    [onClick, day]
-  );
-
-  const [collected, drop] = useDrop(
-    () => ({
-      accept: "recipe",
-      drop: (item: { uuid: RecipeUuid }) => {
-        dispatch(
-          addOrUpdatePlan({
-            date: day,
-            uuid: item.uuid,
-            servingsIncrease: 1,
-          })
-        );
-      },
-      collect: (monitor) => ({
-        isOver: !!monitor.isOver(),
-      }),
-    }),
-    [day]
-  );
-
-  let className = "card";
-  if (collected.isOver || selected) {
-    className += " hoveredBorder";
-  }
-
-  return (
-    <Card className={className} ref={drop} onClick={toggleOnClick}>
-      <CardHeader title={day} className="noSelect" />
-      <CardContent>
-        <div style={{ minHeight: 30, width: 300, flexGrow: 1 }}>
-          {meals &&
-            meals.map((meal) => (
-              <RecipeName key={meal.uuid} meal={meal} day={day} />
-            ))}
-        </div>
-      </CardContent>
-    </Card>
-  );
-};
-
-const RecipeName = ({
-  meal,
-  day,
-}: {
-  meal: IMealPlanItem;
-  day: DateString;
-}) => {
-  const storedMeal = useAppSelector((store) => store.food.recipes[meal.uuid]);
-  const dispatch = useAppDispatch();
-
-  if (!storedMeal) {
-    return null;
-  }
-
-  return (
-    <div style={{ display: "flex", justifyContent: "space-between" }}>
-      <p>{storedMeal.name}</p>
-      <div style={{ display: "flex", alignItems: "center" }}>
-        <Tooltip title={`${meal.servings} serving`}>
-          {/* div instead of fragment as tooltip doesn't work with fragment */}
-          <div
-            style={{
-              display: "flex",
-              alignItems: "center",
-              paddingRight: 6.5,
-              marginLeft: 15,
-            }}
-          >
-            <Typography>{meal.servings}</Typography>
-            <PersonIcon sx={{ paddingRight: 0.5 }} />
-          </div>
-        </Tooltip>
-        <ButtonGroup variant="outlined" sx={{ height: 25 }}>
-          <Button
-            onClick={(event) => {
-              event.preventDefault();
-              dispatch(
-                addOrUpdatePlan({
-                  date: day,
-                  uuid: meal.uuid,
-                  servingsIncrease: -1,
-                })
-              );
-            }}
-          >
-            -
-          </Button>
-          <Button
-            onClick={(event) => {
-              event.preventDefault();
-              dispatch(
-                addOrUpdatePlan({
-                  date: day,
-                  uuid: meal.uuid,
-                  servingsIncrease: 1,
-                })
-              );
-            }}
-          >
-            +
-          </Button>
-        </ButtonGroup>
-        <DeleteIcon
-          onClick={(event) => {
-            event.preventDefault();
-            dispatch(
-              removeFromPlan({
-                date: day,
-                uuid: meal.uuid,
-              })
-            );
-          }}
-          fontSize="small"
-          htmlColor="#7d2020"
-          sx={{ ml: 2 }}
-        />
-      </div>
-    </div>
   );
 };
--- store/reducers/food/meal_plan.ts
+++ store/reducers/food/meal_plan.ts
@@ -1,7 +1,7 @@
 import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 import clone from "just-clone";
 import { IFullStoreState } from "../../store";
-import { RecipeUuid } from './recipes';
+import { RecipeUuid } from "./recipes";
 
 export type DateString = string;
 
@@ -20,56 +20,75 @@ function addDays(theDate: Date, days: number) {
   return new Date(theDate.getTime() + days * 24 * 60 * 60 * 1000);
 }
 
-function createDates(centreDate: Date, pastDays: number, futureDays: number): DateString[] {
-  const offsets = Array.from(Array(pastDays + futureDays).keys()).map(val => val - pastDays)
+function createDates(
+  centreDate: Date,
+  pastDays: number,
+  futureDays: number
+): DateString[] {
+  const offsets = Array.from(Array(pastDays + futureDays).keys()).map(
+    (val) => val - pastDays
+  );
 
-  return offsets.map(dayOffset => {
-    const date = addDays(centreDate, dayOffset)
+  return offsets.map((dayOffset) => {
+    const date = addDays(centreDate, dayOffset);
     // getMonth has indices from 0
-    return `${date.getDate()}/${date.getMonth() + 1}/${date.getFullYear()}`
-  })
+    return `${date.getDate()}/${date.getMonth() + 1}/${date.getFullYear()}`;
+  });
 }
 
 function currentDate(): Date {
-  const time = (new Date()).setHours(12, 0, 0, 0);
+  const time = new Date().setHours(12, 0, 0, 0);
   return new Date(time);
 }
 
 export const mealPlanEmptyState: IMealPlanState = {
-  plan: createDates(currentDate(), 7, 14).reduce((previous, current) => ({
-    ...previous, [current]: []
-  }), {})
-}
+  plan: createDates(currentDate(), 7, 14).reduce(
+    (previous, current) => ({
+      ...previous,
+      [current]: [],
+    }),
+    {}
+  ),
+};
 
-const initialState = mealPlanEmptyState
+const initialState = mealPlanEmptyState;
 
 export const mealPlanSlice = createSlice({
   name: "mealPlanner",
   initialState,
   reducers: {
-    addOrUpdatePlan: (state, action: PayloadAction<{ date: DateString } & { uuid: RecipeUuid, servingsIncrease: number }>) => {
+    addOrUpdatePlan: (
+      state,
+      action: PayloadAction<
+        { date: DateString } & { uuid: RecipeUuid; servingsIncrease: number }
+      >
+    ) => {
       const { date, uuid, servingsIncrease } = action.payload;
       if (state.plan[date]) {
-        const index = state.plan[date].findIndex(plan => plan.uuid === uuid)
+        const index = state.plan[date].findIndex((plan) => plan.uuid === uuid);
         if (index > -1) {
-          const newServings = state.plan[date][index].servings + servingsIncrease;
+          const newServings =
+            state.plan[date][index].servings + servingsIncrease;
           if (newServings <= 0) {
             state.plan[date].splice(index, 1);
           } else {
             state.plan[date][index] = {
               uuid,
-              servings: newServings
+              servings: newServings,
             };
           }
         } else {
           state.plan[date].push({
             uuid,
-            servings: 1
+            servings: 1,
           });
         }
       }
     },
-    removeFromPlan: (state, action: PayloadAction<{ date: DateString } & { uuid: RecipeUuid }>) => {
+    removeFromPlan: (
+      state,
+      action: PayloadAction<{ date: DateString } & { uuid: RecipeUuid }>
+    ) => {
       const { date, uuid } = action.payload;
       const newPlan = clone(state.plan);
 
@@ -77,20 +96,22 @@ export const mealPlanSlice = createSlice({
         return state;
       }
 
-      const index = newPlan[date].findIndex(plan => plan.uuid === uuid)
+      const index = newPlan[date].findIndex((plan) => plan.uuid === uuid);
       if (index > -1) {
         newPlan[date].splice(index, 1);
       }
 
       return { ...state, plan: newPlan };
-    }
+    },
   },
   extraReducers: {
     "user/login": (state, action: PayloadAction<IFullStoreState>) => {
-      const mealPlan = clone(mealPlanEmptyState)
+      const mealPlan = clone(mealPlanEmptyState);
 
-      if (action.payload.mealPlan) {
-        for (const [date, plan] of Object.entries(action.payload.mealPlan)) {
+      if (action.payload.mealPlan.plan) {
+        for (const [date, plan] of Object.entries(
+          action.payload.mealPlan.plan
+        )) {
           if (date in mealPlan) {
             mealPlan.plan[date] = plan;
           }
@@ -102,8 +123,8 @@ export const mealPlanSlice = createSlice({
     "user/logout": (state) => {
       return initialState;
     },
-  }
-})
+  },
+});
 
 export const { addOrUpdatePlan, removeFromPlan } = mealPlanSlice.actions;
 
