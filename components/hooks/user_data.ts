5099835ec759145b80cef65735fbebb320c6b92e
George Sheppard <georgesheppard98@gmail.com>
2022-12-18 18:37:16 +0000

    Revert "Updates to user_data"
    This reverts commit 297ad62466bab929cd4ca3a201b60817bf5d10e4.

--- components/hooks/user_data.ts
+++ components/hooks/user_data.ts
@@ -47,23 +47,64 @@ export const useUserData = (): IUserDataReturn => {
   const [gotUserData, setGotUserData] = useState(false);
   const [uploading, setUploading] = useState(false);
 
-  // Only need to save if a user is logged in
-  const canUpload = !!session?.id && gotUserData;
+  const canUpload = useMemo(() => {
+    // Only need to save if a user is logged in
+    return !!session?.id && gotUserData;
+  }, [session?.id, gotUserData]);
 
   useEffect(() => {
-    const fetchUserData = async () => {
-      const { failed, json } = await attemptToFetchUserProfile(session.id);
+    let timerId: NodeJS.Timeout;
 
-      if (failed) {
-        setOffline(true);
-        return;
-      } else if (offline) {
-        setOffline(false);
+    const fetchUserData = async () => {
+      let json: IFullStoreState | undefined = undefined;
+      try {
+        const profileResults = await AwsS3Client.send(
+          new ListObjectsCommand({
+            Bucket: process.env.ENV_AWS_S3_BUCKET_NAME,
+            Prefix: `${session.id}/profile.json`,
+            MaxKeys: 1,
+          })
+        );
+
+        if (profileResults.$metadata.httpStatusCode !== 200) {
+          setOffline(true);
+        }
+
+        if (profileResults.$metadata.httpStatusCode === 200) {
+          if (offline) {
+            setOffline(false);
+          }
+
+          // User doesn't have a profile, in this case we should stop trying to fetch a profile
+          // and create a new one by dispatching an empty profile
+          if (
+            !profileResults?.Contents ||
+            profileResults.Contents?.length === 0
+          ) {
+            json = {
+              user: userEmptyState,
+              printing: printingEmptyState,
+              plants: plantsEmptyState,
+              food: recipesEmptyState,
+              mealPlan: mealPlanEmptyState,
+            } as IFullStoreState;
+          }
+
+          const profileUrl = await getS3SignedUrl(`${session.id}/profile.json`);
+          const data = await fetch(profileUrl);
+          if (data.ok) {
+            json = await data.json();
+          }
+        }
+      } catch (err) {
+        console.error(err);
       }
 
       if (json && !gotUserData) {
         setGotUserData(true);
         dispatch(login(json));
+      } else {
+        timerId = setTimeout(() => fetchUserData(), 20000);
       }
     };
 
@@ -76,6 +117,12 @@ export const useUserData = (): IUserDataReturn => {
     if (session?.id && !gotUserData) {
       fetchUserData();
     }
+
+    return () => {
+      if (timerId) {
+        clearTimeout(timerId);
+      }
+    };
   }, [session?.id, gotUserData, dispatch, offline, setOffline]);
 
   const storeUserData = useCallback(() => {
@@ -101,64 +148,16 @@ export const useUserData = (): IUserDataReturn => {
     // the properties
   }, [uploadFile, canUpload]);
 
-  // Save the user data if the user closes the webpage
+  // Save the user data everytime the route changes
+  // And if the user closes the webpage
   useEffect(() => {
+    router.events.on("routeChangeStart", storeUserData);
     window.addEventListener("beforeunload", storeUserData);
     return () => {
+      router.events.off("routeChangeStart", storeUserData);
       window.removeEventListener("beforeunload", storeUserData);
     };
   }, [storeUserData, router.events]);
 
   return { upload: storeUserData, uploading, canUpload, offline };
-};
-
-interface IProfileResults {
-  failed?: boolean;
-  json?: IFullStoreState;
-}
-
-const attemptToFetchUserProfile = async (
-  sessionId: string
-): Promise<IProfileResults> => {
-  const profileResults = await AwsS3Client.send(
-    new ListObjectsCommand({
-      Bucket: process.env.ENV_AWS_S3_BUCKET_NAME,
-      Prefix: `${sessionId}/profile.json`,
-      MaxKeys: 1,
-    })
-  );
-
-  if (profileResults.$metadata.httpStatusCode !== 200) {
-    return { failed: true };
-  }
-
-  // User doesn't have a profile, in this case we should stop trying to fetch a profile
-  // and create a new one by dispatching an empty profile
-  if (!profileResults?.Contents || profileResults.Contents?.length === 0) {
-    return {
-      json: {
-        user: userEmptyState,
-        printing: printingEmptyState,
-        plants: plantsEmptyState,
-        food: recipesEmptyState,
-        mealPlan: mealPlanEmptyState,
-      },
-    };
-  }
-
-  const profileUrl = await getS3SignedUrl(`${sessionId}/profile.json`);
-  const data = await fetch(profileUrl);
-
-  if (!data.ok) {
-    console.error("Response error status", data.statusText);
-    return { failed: true };
-  }
-
-  const json = await data.json();
-  if (!json) {
-    console.error("No json for user profile");
-    return { failed: true };
-  }
-
-  return { json };
 };
