a97bfd0eb5b6e6c9d8fdd96d2de5ad3f673f5410
George Sheppard <gsheppard@spotify.com>
2022-12-27 22:09:25 +0100

    More food form improvements

--- components/pages/recipes/recipe_form/component_form.tsx
+++ components/pages/recipes/recipe_form/component_form.tsx
@@ -1,20 +1,17 @@
-import DeleteIcon from "@mui/icons-material/Delete";
 import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
 import Accordion from "@mui/material/Accordion";
 import AccordionDetails from "@mui/material/AccordionDetails";
 import AccordionSummary from "@mui/material/AccordionSummary";
-import Checkbox from "@mui/material/Checkbox";
 import Divider from "@mui/material/Divider";
 import FormControl from "@mui/material/FormControl";
-import FormControlLabel from "@mui/material/FormControlLabel";
-import IconButton from "@mui/material/IconButton";
-import TextField from "@mui/material/TextField";
-import Tooltip from "@mui/material/Tooltip";
-import { ChangeEvent, useState, MouseEvent } from "react";
+import { useState } from "react";
 import { useBoolean } from "../../../hooks/use_boolean";
 import { ComponentsFormData } from "./component_form_data";
 import { IngredientsInputTable } from "./ingredients/ingredients_table";
 import { InstructionInputList } from "./instructions/instructions_list";
+import { NameRow } from "./name_row";
+import { ServingsTextField } from "./servings";
+import { StoreableCheckbox } from "./storeable";
 
 export interface IComponentFormProps {
   uuid: string;
@@ -35,33 +32,14 @@ export const ComponentForm = (props: IComponentFormProps) => {
   return (
     <Accordion>
       <AccordionSummary expandIcon={<ExpandMoreIcon />}>
-        <TextField
-          fullWidth
-          value={name}
-          label="Section name"
-          id="name"
-          variant="standard"
-          margin="none"
-          onClick={(event: MouseEvent<HTMLElement>) => {
-            event.stopPropagation();
-          }}
-          onChange={(event: ChangeEvent<HTMLTextAreaElement>) => {
-            component.name = event.target.value;
-            setName(event.target.value);
-          }}
+        <NameRow
+          componentFormData={componentFormData}
+          forceUpdate={forceUpdate}
+          component={component}
+          setName={setName}
+          componentId={uuid}
+          name={name}
         />
-        <div style={{ flexGrow: 1 }} />
-        <IconButton
-          onClick={(event) => {
-            event.stopPropagation();
-            delete componentFormData.components[uuid];
-            forceUpdate();
-          }}
-          size="small"
-          sx={{ alignSelf: "center" }}
-        >
-          <DeleteIcon fontSize="small" />
-        </IconButton>
       </AccordionSummary>
       <AccordionDetails>
         <Divider textAlign="center">Ingredients</Divider>
@@ -80,37 +58,16 @@ export const ComponentForm = (props: IComponentFormProps) => {
           Optional
         </Divider>
         <FormControl sx={{ pt: 1, flexDirection: "row", alignItems: "end" }}>
-          <TextField
-            label="Servings"
-            value={servings}
-            id="instruction"
-            variant="standard"
-            type="number"
-            margin="none"
-            onChange={(event: ChangeEvent<HTMLTextAreaElement>) => {
-              const newServings = parseInt(event.target.value, 10);
-              const validServings = isNaN(newServings) ? 1 : newServings;
-              component.servings = validServings;
-              setServings(validServings);
-            }}
-            sx={{ width: "100px" }}
+          <ServingsTextField
+            setServings={setServings}
+            servings={servings}
+            component={component}
           />
-          <Tooltip title="Can it last in the cupboard, or freezer">
-            <FormControlLabel
-              control={
-                <Checkbox
-                  checked={storeable}
-                  onChange={(event: React.ChangeEvent<HTMLInputElement>) => {
-                    component.storeable = event.target.checked;
-                    setters.setState(event.target.checked);
-                  }}
-                  size="small"
-                />
-              }
-              label="Storeable"
-              sx={{ ml: 5 }}
-            />
-          </Tooltip>
+          <StoreableCheckbox
+            component={component}
+            storeable={storeable}
+            setState={setters.setState}
+          />
         </FormControl>
       </AccordionDetails>
     </Accordion>
