0e96ff4d48d5dc365b017768eabbadf7b3f933c5
George Sheppard <gsheppard@spotify.com>
2023-04-17 17:53:29 +0100

    Remove models

--- components/hooks/user_data/query_keys.ts
+++ components/hooks/user_data/query_keys.ts
@@ -1,19 +1,12 @@
 import { RecipeUuid } from "../../../store/reducers/food/recipes/types";
-import { ModelUuid } from "../../../store/reducers/printing/types";
 
-export const shared = "shared"
+export const shared = "shared";
 
 export const sessionQueryKey = (userId: string) => [userId];
 export const recipesQueryKey = (userId: string) => [userId, "R-"];
 export const recipeQueryKey = (id: RecipeUuid, userId: string) => [
   userId,
   "R-",
-  id,
-];
-export const modelsQueryKey = (userId: string) => [userId, "M-"];
-export const modelQueryKey = (id: ModelUuid, userId: string) => [
-  userId,
-  "M-",
   id,
 ];
 export const mealPlanQueryKey = (userId: string) => [userId, "MP"];
