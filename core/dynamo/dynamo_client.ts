c02c7ec177eb0f8e829f82fafd3385df42abdad6
George Sheppard <georgesheppard98@gmail.com>
2023-10-02 08:01:26 +0100

    No retries and add logger

--- core/dynamo/dynamo_client.ts
+++ core/dynamo/dynamo_client.ts
@@ -14,6 +14,8 @@ export const AwsDynamoClient = new DynamoDBClient({
     accessKeyId: process.env.ENV_AWS_DYNAMO_ACCESS_KEY ?? "",
     secretAccessKey: process.env.ENV_AWS_DYNAMO_SECRET_ACCESS_KEY ?? "",
   },
+  logger: console,
+  maxAttempts: 1,
 });
 
 export const AwsDynamoDocClient = DynamoDBDocument.from(AwsDynamoClient, {
@@ -21,3 +23,4 @@ export const AwsDynamoDocClient = DynamoDBDocument.from(AwsDynamoClient, {
     removeUndefinedValues: true,
   },
 });
+console.log('after dynamo client')
--- server/index.ts
+++ server/index.ts
@@ -1,10 +1,7 @@
-import "../core/dynamo/dynamo_client";
-import "../core/s3/s3_client";
 import { mealPlanRouter } from "./routers/mealPlan";
 import { recipesRouter } from "./routers/recipes";
 import { s3Router } from "./routers/s3";
 import { publicProcedure, router } from './trpc';
-
 
 export const appRouter = router({
   testData: publicProcedure.query(() => ({ test: 'data' })),
